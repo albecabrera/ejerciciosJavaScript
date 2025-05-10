895a6cb279a9499fbe95c1a334d90e2554a69dec
albecabrera <albe.cabrera@yahoo.de>
2025-05-10 09:48:23 +0200

    first commit

---
+++ condicinales/switch.js
@@ -0,0 +1,22 @@
+// switch 
+let codigo = 1515
+let codigoSeguridad
+
+switch (codigo) {
+    case codigoSeguridad = 1515:
+        console.log("Atención, la puerta se va a abrir!");
+    break
+    case codigoSeguridad = 2020:
+        console.log("Código incorrecto, acceso denegado.");
+    break
+    case codigoSeguridad = 3030:
+        console.log("Código de emergencia activado.");
+    break
+    case codigoSeguridad = 4040:
+        console.log("Código no reconocido, intente nuevamente.");
+    break
+    default:
+        console.log("Error: Código no válido.");
+    break
+}
+
