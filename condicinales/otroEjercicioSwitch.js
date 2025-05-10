0ed147f08d6231b4332e8321437cce34755c2abf
albecabrera <albe.cabrera@yahoo.de>
2025-05-10 23:47:16 +0200

    operador tenrnario

--- condicinales/otroEjercicioSwitch.js
+++ condicinales/otroEjercicioSwitch.js
@@ -1,24 +1,23 @@
 // Ejercicio 3 switch
-// Escribe un programa que use un switch para determinar el tipo de animal
+// Escribe un programa que use un switch para determinar el tipo de animal vamos a ver que pasa si sigo escribiendo y llego hasta aqui
 // según el sonido que hace. Por ejemplo, "guau" es un perro,
 // "miau" es un gato, etc.
 // Agrega al menos 4 casos diferentes y un caso por defecto.
-let sonidoAnimal = "miau"
+let sonidoAnimal = "eh ";
 
 switch (sonidoAnimal) {
     case "guau":
         console.log("Eres un perro");
-        break
+        break;
     case "miau":
         console.log("Eres un gato");
-        break
+        break;
     case "brumm":
         console.log("Eres una abejita");
-        break
+        break;
     case "pio pio":
         console.log("Eres un pollito");
-        break
+        break;
     default:
         console.log("No eres ningún animal, eres una persona 'Cretino' ");
-
 }
---
+++ operadorTernario.js
@@ -0,0 +1,15 @@
+let usuario = "Emilia";
+
+let autorizado = usuario === "Emilia" ? 'Autorizado' : 'No autorizado';
+console.log(autorizado);
+
+// Ejercicio para el condicional ternario
+let edad = 12;
+let puedeConducir = edad >= 18 ? 'Puede conducir' : 'No puede conducir';
+console.log(puedeConducir);
+
+// Ejercicio para practicar el operador ternario
+// Determina si un número es par o impar usando un operador ternario
+let numero = 6;
+let numeroImpar = numero % 2 === 1 ? 'El número es impar' : 'El número es par';
+console.log(numeroImpar);
